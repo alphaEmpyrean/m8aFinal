eee5deafb735118bfc7a9b642463fd54b2a87646
alphaEmpyrean <dev@alphaempyrean.com>
2022-11-15 13:06:16 -0600

    Added in Morgan middleware

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 // Import ExpressJS express() top level function
 const express = require('express');
+// Import Morgan middleware
+const morgan = require('morgan');
 // Import routes
 const router = require('./routes/routes');
 
 // Instantiate an application instance
 const app = express();
 
-// Add a route to the application
+// ******** Configure middleware ********
+// Log HTTP requests -https://github.com/expressjs/morgan#readme
+app.use(morgan('dev'));
+
+// ******** Configure routes ********
 app.use('/', router);
 
 // Make app instance avaliable for import
