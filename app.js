41903df4fd684cf38374d2f7857b05908c08a06e
alphaEmpyrean <dev@alphaempyrean.com>
2022-11-16 00:03:48 -0600

    Added connection to mongodb via mongoose
    Updated the app configuration to include a connection to mongodb hosted db. Connection details listed in args file 'config.env'.

--- app.js
+++ app.js
@@ -2,13 +2,26 @@ const express = require('express');
 const morgan = require('morgan');
 const router = require('./routes/routes');
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 // Instantiate an application instance
 const app = express();
 
-// ******** Configure Application Instance ********
+// ******** APP CONFIG ********
 // Set path to args file
 dotenv.config({path: './config.env' });
+
+// ******** database ********
+// Connect to mongodb
+mongoose.connect(process.env.MONGODB_CLUSTER_URI
+    .replace('<password>', process.env.DB_PASSWORD))
+    .then(
+        () => {
+            // Set database
+            mongoose.connection = mongoose.connection.useDb(process.env.DB_NAME);
+            console.log("INFO - Successfully connected to database");},
+        err => { console.log("ERROR - " + err) }
+    );
 
 // ******** middleware ********
 app.use(morgan('dev'));
@@ -16,5 +29,4 @@ app.use(morgan('dev'));
 // ******** routes ********
 app.use('/', router);
 
-// Make app instance avaliable for import
 module.exports = app;
